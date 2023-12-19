f1b4a2004f49462f2580db74706a47cd7ef30a4b
piop2 <seungyounyou0220@gmail.com>
2023-12-20 02:37:50 +0900

    tailwind setting

--- meta-app/tailwind.config.js
+++ meta-app/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      "tablet": "640px",
+      "laptop": "1024px",
+      "desktop": "1280px"
+    },
     extend: {},
+    colors: {
+      black: {
+        DEFAULT: "#000000",
+        gear: "#1E1E1E"
+      },
+      gray: {
+        light: "#A7A7AC",
+        dim: "#696969"
+      },
+    }
   },
   plugins: [],
 }
