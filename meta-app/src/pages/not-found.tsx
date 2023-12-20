6498169ad465b39e3f8609510c2489c9b2e67969
piop2 <seungyounyou0220@gmail.com>
2023-12-20 11:59:56 +0900

    not-found.tsx: add text

--- meta-app/src/pages/not-found.tsx
+++ meta-app/src/pages/not-found.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import HeaderBar from "../components/header-bar";
+import Footer from "../components/footer";
 
 export default function NotFound() {
-    return (<div>Not Found</div>);
+    return (
+        <Fragment>
+            <HeaderBar/>
+            <div className={"mt-16 h-160 flex flex-col items-center"}>
+                <div className={"mt-10 h-fit font-open-sans text-8xl tracking-widest"}>
+                    404
+                </div>
+                <div className={"mt-5 font-noto-sans font-bold"}>
+                    Page Not Found...
+                </div>
+            </div>
+            <Footer/>
+        </Fragment>
+    );
 };
