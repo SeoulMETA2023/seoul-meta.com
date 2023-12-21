83d79297e36a92177c847a9ee3a35f648bc84892
piop2 <seungyounyou0220@gmail.com>
2023-12-21 14:35:47 +0900

    index.css: header list height update

--- meta-app/src/components/header-bar.tsx
+++ meta-app/src/components/header-bar.tsx
@@ -28,25 +28,30 @@ export default function HeaderBar() {
                     <Link to={"/"} onClick={headerOff}>
                         <img src={"image/meta_full(white).svg"} alt={"meta logo"} className={`h-full duration-100 ${active? "opacity-0" : "opacity-100"}`}/>
                     </Link>
-                    <svg className={"w-fit h-full"} width="400" height="400" viewBox="0 0 40 40" onClick={headerToggle}>
-                        <path className={`duration-300 ${active? "translate-y-2" : ""}`} fill="#FFFFFF" fillRule="evenodd" clipRule="evenodd"
-                              d="M33.3327 10H6.66602V15H33.3327V10Z"></path>
-                        <path fill="#FFFFFF" fillRule="evenodd" clipRule="evenodd"
-                              d="M6.66602 18.3317H33.3327V23.3317H6.66602V18.3317Z"></path>
-                        <path className={`duration-300 ${active? "-translate-y-2" : ""}`} fill="#FFFFFF" fillRule="evenodd" clipRule="evenodd"
-                              d="M6.66602 26.665H33.3327V31.665H6.66602V26.665Z"></path>
-                    </svg>
+                    <button onClick={headerToggle}>
+                        <svg className={"w-fit h-full"} width="400" height="400" viewBox="0 0 40 40">
+                            <path className={`duration-300 ${active ? "translate-y-2" : ""}`} fill="#FFFFFF"
+                                  fillRule="evenodd" clipRule="evenodd"
+                                  d="M33.3327 10H6.66602V15H33.3327V10Z"></path>
+                            <path fill="#FFFFFF" fillRule="evenodd" clipRule="evenodd"
+                                  d="M6.66602 18.3317H33.3327V23.3317H6.66602V18.3317Z"></path>
+                            <path className={`duration-300 ${active ? "-translate-y-2" : ""}`} fill="#FFFFFF"
+                                  fillRule="evenodd" clipRule="evenodd"
+                                  d="M6.66602 26.665H33.3327V31.665H6.66602V26.665Z"></path>
+                        </svg>
+                    </button>
                 </div>
             </header>
-            <div className={`w-full ${active? "h-screen" : "h-16"} pt-16 fixed top-0 bg-black-gear duration-700 ease-in-out overflow-hidden z-40`}>
+            <div
+                className={`w-full ${active ? "h-screen" : "h-16"} pt-16 fixed top-0 bg-black-gear duration-700 ease-in-out overflow-hidden z-40`}>
                 <div className={"mr-12 ml-12 mt-5 flex flex-col text-white font-noto-sans"}>
                     <Link to={"/"}
-                          className={`header-list ${active? "opacity-100" : "opacity-0"} ${location.pathname === "/"? "underline underline-offset-8" : ""}`}
+                          className={`header-list ${active ? "opacity-100" : "opacity-0"} ${location.pathname === "/" ? "underline underline-offset-8" : ""}`}
                           onClick={headerOff}>
                         Who are we?
                     </Link>
                     <Link to={"/team"}
-                          className={`header-list ${active? "opacity-100" : "opacity-0"} ${location.pathname === "/team"? "underline underline-offset-8" : ""}`}
+                          className={`header-list ${active ? "opacity-100" : "opacity-0"} ${location.pathname === "/team" ? "underline underline-offset-8" : ""}`}
                           onClick={headerOff}>
                         Team
                     </Link>
